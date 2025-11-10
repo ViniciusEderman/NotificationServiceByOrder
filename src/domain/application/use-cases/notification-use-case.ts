f4579a8f0873d64b6dd8c1b13d81dc6e32b3e1fa
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2025-11-10 20:13:18 -0300

    Breaking down the use-case into others, to follow the S of SOLID, in addition to adjusting the structure

--- src/domain/application/use-cases/notification-use-case.ts
+++ src/domain/application/use-cases/notification-use-case.ts
@@ -1,64 +1,27 @@
 import { DomainNotification } from "@/domain/enterprise/entities/notification";
 import { Logger } from "@/domain/interfaces/logger";
 import { NotificationSender } from "@/domain/interfaces/notification-sender";
-import { NotificationGateway } from "@/domain/interfaces/queue";
-import { NotificationRepository } from "@/domain/interfaces/notification-repository";
-
-const MAX_TRIES = 5;
+import { NotificationCreationPublisher } from "@/domain/application/use-cases/notification-creation-publisher";
+import { NotificationRetryScheduler } from "@/domain/application/use-cases/notification-retry-scheduler";
 
 export class SendNotificationUseCase {
   constructor(
     private logger: Logger,
     private sender: NotificationSender,
-    private gateway: NotificationGateway,
-    private repositorie: NotificationRepository
+    private retryScheduler: NotificationRetryScheduler,
+    private creationPublisher: NotificationCreationPublisher,
   ) {}
 
-  async execute(notification: DomainNotification) {
-    this.logger.info("transmitting a notification...", {
-      status: notification.status,
-      recipient: notification.recipient,
-      channel: notification.channel,
-      id: notification.id,
-    });
+  async execute(notification: DomainNotification): Promise<void> {
+    this.logger.info("transmitting notification...", { id: notification.id });
 
-    const sendMessageResult = await this.sender.send(notification);
+    const result = await this.sender.send(notification);
 
-    if (!sendMessageResult.isSuccess) {
-      notification.tries = (notification.tries || 0) + 1;
-
-      if (notification.tries > MAX_TRIES) {
-        this.logger.error("", {
-          status: notification.status,
-          recipient: notification.recipient,
-          channel: notification.channel,
-          id: notification.id,
-        });
-
-        await this.repositorie.save(notification);
-        return;
-      }
-
-      this.logger.info("");
-      const retryResult = await this.gateway.publishToRetry(notification);
-
-      if (!retryResult.isSuccess) {
-        this.logger.error("");
-      }
+    if (!result.isSuccess) {
+      await this.retryScheduler.handleFailure(notification);
       return;
     }
 
-    const publishResult = await this.gateway.publishToCreate(notification);
-    if (!publishResult.isSuccess) {
-      this.logger.error("", {
-        status: notification.status,
-        recipient: notification.recipient,
-        channel: notification.channel,
-        id: notification.id,
-      });
-
-      await this.repositorie.save(notification);
-      return;
-    }
+    await this.creationPublisher.handleSuccess(notification);
   }
 }
